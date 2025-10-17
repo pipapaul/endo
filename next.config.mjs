b14551171016ee9d7e6fd7e7dcdf478d33d9a05c
pipapaul <mail@pauletzel.de>
2025-10-17 10:28:14 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',         // ← aktiviert Static Export beim Build
+  trailingSlash: true,      // saubere /route/ → /route/index.html
+  images: { unoptimized: true }, // <Image> ohne Optimizer
 };
-
 export default nextConfig;
